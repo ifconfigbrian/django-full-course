3e4c2c9f98d28039171f3332d0a5af7d9c25b793
ifconfigbrian <njugunabriian@gmail.com>
2024-10-03 15:24:24 +0300

    created a posts app

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage, name='homepage'),
     path('about/', views.about, name='aboutpage'),
+    path('posts/', include('posts.urls'))
 ]
