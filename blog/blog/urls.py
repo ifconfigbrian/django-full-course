3b8d0096ad6343d9d7585285d7c01953249df8de
ifconfigbrian <njugunabriian@gmail.com>
2024-10-08 10:38:48 +0300

    migrations stuff and other stuff

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +10,7 @@ urlpatterns = [
     path('about/', views.about, name='aboutpage'),
     path('posts/', include('posts.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
 def posts_list(request):
-    return render(request, 'posts/posts_list.html')
+    posts = Post.objects.all().order_by('-date')
+    return render(request,'posts/posts_list.html',{'posts':posts})
+
+def post_page(request,slug):
+    post = get_object_or_404(Post,slug=slug)
+    return render(request,'posts/post_page.html',{'post':post})
